f600cae3a052e8ece5d0e5410b135ebb70d9a759
jiten228 <jiten228@yahoo.com>
2020-11-10 11:18:58 -0600

    First Java Program to initial commit on Github

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // let's make more changes after initial commit
+        System.out.println("it's time to make more change and commit");
     }
 }
